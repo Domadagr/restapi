c8f3a606e4b626f78dbbd64e6fce60c31a355386
HelDraugar <91600617+HelDraugar@users.noreply.github.com>
2025-01-10 10:03:46 +0100

    added try/catch

--- week2/js/data/books.js
+++ week2/js/data/books.js
@@ -2,14 +2,24 @@
 const sql = require('../db.js');
 
 const getBooklist = async () => {
-    return await sql`select * from books`;
+    try {
+        const booklist = await sql`select * from books`;
+        return booklist;
+    } catch (error) {
+        throw new Error("Failed to get list, try again later");
+    }
 };
 
 const addBook = async (book) => {
-    return await sql`
-    INSERT INTO books (title, author, year, genre)
-    VALUES (${book.title}, ${book.author}, ${book.year}, ${book.genre})
-    RETURNING *`;
+    try {
+        const result = await sql`
+        INSERT INTO books (title, author, year, genre)
+        VALUES (${book.title}, ${book.author}, ${book.year}, ${book.genre})
+        RETURNING *`;
+        return result;
+    } catch (error) {
+        throw new Error("Failed to get list, try again later");
+    }
 };
 
 const verifyPayload = ((req, res, next) => {
@@ -21,29 +31,41 @@ const verifyPayload = ((req, res, next) => {
 });
 
 const patchBook = async (reqID, patch) => {
-    const [updateBook] = await sql`
-    UPDATE books
-    SET title = ${patch.title}, author = ${patch.author}, year = ${patch.year}, genre = ${patch.genre}
-    WHERE id = ${reqID}
-    RETURNING *`;
-    return updateBook;
+    try {
+        const [updateBook] = await sql`
+        UPDATE books
+        SET title = ${patch.title}, author = ${patch.author}, year = ${patch.year}, genre = ${patch.genre}
+        WHERE id = ${reqID}
+        RETURNING *`;
+        return updateBook;
+    } catch (error) {
+        throw new Error("Failed to get list, try again later");
+    } 
 };
 
 const getBook = async (id) => {
-    const [book] = await sql`
+    try {
+        const [book] = await sql`
         SELECT * FROM books
         WHERE id = ${id}`;
 
     return book;
+    } catch (error) {
+        throw new Error("Failed to get list, try again later");
+    } 
 };
 
 const deleteBook = async (id) => {
-    const [book] = await sql`
-    DELETE FROM books
-    WHERE id = ${id}
-    RETURNING *`;
-
-    return book;
+    try {
+        const [book] = await sql`
+        DELETE FROM books
+        WHERE id = ${id}
+        RETURNING *`;
+    
+        return book;
+    } catch (error) {
+        throw new Error("Failed to get list, try again later");
+    } 
 };
 
 module.exports = { getBooklist, addBook, verifyPayload, patchBook, getBook, deleteBook };
